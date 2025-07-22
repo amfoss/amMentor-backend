30b5216db8ceb6eec41cde075df64b86034e4513
GaniDande <142842955+ganidande905@users.noreply.github.com>
2025-07-22 22:47:21 +0530

    added the role

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -21,6 +21,7 @@ def register_user(user: UserCreate, db: Session = Depends(get_db)):
     new_user = models.User(
         email=user.email,
         name=user.name,
+        role=user.role
     )
     db.add(new_user)
     db.commit()
